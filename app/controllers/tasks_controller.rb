0e91d617a7cdc64dd52bcfd11f04b2e1db2de87b
silvia sartor <silvia.maria.sartor@gmail.com>
2019-03-05 16:25:18 +0100

    optional done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,7 +28,7 @@ class TasksController < ApplicationController
     redirect_to task_path(@task.id)
   end
 
-  def delete
+  def destroy
     @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
